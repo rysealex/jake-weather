26bf5b40f7fba1a38c2f0d4fbae53ef4451c2fea
Alex Ryse <aryse54@gmail.com>
2025-11-23 01:33:43 -0800

    Created add user POST model and route

--- back/models/user_model.py
+++ back/models/user_model.py
@@ -27,3 +27,31 @@ class UserModel:
                 cursor.close()
             if conn:
                 conn.close()
+
+    """Function to add a new user to the jake weather database"""
+    def add_user(self, username, fname, lname, email, password, country, city, state, zip):
+        conn = None
+        cursor = None
+        try:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            sql = """
+            INSERT INTO user (username, fname, lname, email, password, country, city, state, zip)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            user_data = (username, fname, lname, email, password, country, city, state, zip)
+            cursor.execute(sql, user_data)
+            conn.commit() # Commit the transaction
+            return cursor.lastrowid # Return the ID of the newly created user
+        except Error as e:
+            print(f"Error creating user: {e}")
+            if conn:
+                conn.rollback()
+            return None
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
--- back/routes/user_routes.py
+++ back/routes/user_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+import bcrypt
+from flask import Blueprint, jsonify, request
 from models.user_model import UserModel
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
@@ -13,3 +14,34 @@ def get_all_users():
         return jsonify(users), 200
     else:
         return jsonify({"message": "Failed to fetch users"}), 500
+    
+@user_bp.route('/add', methods=['POST'])
+def add_user():
+    """Endpoint to add a new user"""
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No input data provided"}), 400
+
+    username = data.get('username')
+    fname = data.get('fname')
+    lname = data.get('lname')
+    email = data.get('email')
+    password = data.get('password')
+    country = data.get('country')
+    city = data.get('city')
+    state = data.get('state')
+    zip = data.get('zip')
+
+    if not all([username, fname, lname, email, password, country, city, state, zip]):
+        return jsonify({"error": "All fields are required"}), 400
+    
+    # hash the password before storing in jake weather db
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    user_id = user_model.add_user(username, fname, lname, email, hashed_password, country, city, state, zip)
+
+    if user_id:
+        return jsonify({"message": "User added successfully", "user_id": user_id}), 201
+    else:
+        return jsonify({"error": "Failed to add user"}), 500

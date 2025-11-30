135f30ce18c7a11189b0bb243c598e5aa70844ea
Alex Ryse <aryse54@gmail.com>
2025-11-29 16:34:19 -0800

    Revert "Oscar clutch weather map"
    This reverts commit 7b389713f9e6d8a4a6c2d482129cf88d09c26104.

--- front/src/comp/weathermap.jsx
+++ front/src/comp/weathermap.jsx
@@ -12,8 +12,6 @@ function Weathermap() {
 
 	// useEffect to fetch user's geolocation
 	useEffect(() => {
-		console.log("API Key:", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
-		console.log("MAP ID:", process.env.REACT_APP_GOOGLE_MAPS_MAP_ID);
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(
 				(position) => {
@@ -41,11 +39,11 @@ function Weathermap() {
 						Error fetching geolocation: {geolocationError}
 					</div>
 				)}
-				<APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+				<APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
 					<Map
 						defaultCenter={userPos || mapPos}
 						defaultZoom={15}
-						mapId={process.env.REACT_APP_GOOGLE_MAPS_MAP_ID}
+						mapId={process.env.GOOGLE_MAPS_MAP_ID}
 					>
 						{userPos && (
 							<AdvancedMarker
